11629f623ed7d614e5d10680d6989dcc1ac4a404
Peter Pakos <peter.pakos@wandisco.com>
2016-03-16 20:55:43 +0000

    Replaced getopt with argparse

--- python_template.py
+++ python_template.py
@@ -7,7 +7,7 @@
 from __future__ import print_function
 from sys import stderr, argv
 from os import path
-from getopt import getopt, GetoptError
+from argparse import ArgumentParser
 
 
 class Main(object):
@@ -15,40 +15,23 @@ class Main(object):
     __name = path.basename(argv[0])
 
     def __init__(self):
-        self.parse_options()
+        self.parse_args()
         print('Hello World!')
 
     def __del__(self):
         return True
 
-    def parse_options(self):
-        options = None
-
-        try:
-            options, args = getopt(argv[1:], 'hv', [
-                'help',
-                'version'
-            ])
-        except GetoptError as err:
-            self.die(err)
-
-        for opt, arg in options:
-            if opt in ('-v', '--version'):
-                self.display_version()
-                exit()
-            if opt in ('-h', '--help'):
-                self.display_usage()
-                exit()
+    def parse_args(self):
+        parser = ArgumentParser()
+        parser.add_argument('-v', '--version',
+                            help='show version', action="store_true")
+        args = parser.parse_args()
+        if args.version:
+            self.display_version()
+            exit()
 
     def display_version(self):
         print('%s version %s' % (self.__name, self.__version))
-
-    def display_usage(self):
-        self.display_version()
-        print('''Usage: %s [OPTIONS]
-AVAILABLE OPTIONS:
--h, --help      Print this help summary page
--v, --version   Print version number''' % self.__name)
 
     @staticmethod
     def die(message=None, code=1):
